4c717ac0830f547662dc554f6ee716330080365e
Anil Sapkota <45574896+anilsapkota@users.noreply.github.com>
2026-05-03 01:23:54 +0000

    create a dimensional table for date

---
+++ star_schema_query.sql
@@ -0,0 +1,20 @@
+select * 
+from sales_data;
+
+
+create table sales_data_cleaned as 
+select * 
+from sales_data;
+
+update sales_data_cleaned
+set OrderDate = replace(replace(OrderDate , '"',''), '''','');
+
+create table dim_date as 
+select distinct OrderDate
+  , strftime('%Y',OrderDate) as Year
+  ,strftime('%m',OrderDate) as Month
+  ,strftime('%d',OrderDate) as Day
+  ,strftime('%w',OrderDate) as DayofWeek
+  ,strftime('%w',OrderDate) as WeekNumber
+  , case strftime('%w', OrderDate) when '0' then 1 when '6' then 1 else 0 end as IsWeekend
+from sales_data_cleaned;
